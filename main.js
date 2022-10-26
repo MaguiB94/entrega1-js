14f9959b5f598d9afd4f452236af74b65c49fd1d
Magui Blanco <maggui.b@live.com>
2022-10-26 16:21:33 -0300

    entrega1

---
+++ main.js
@@ -0,0 +1,47 @@
+const miLista = [
+    {
+        id: 1,
+        nombre: "pizzaMuzza",
+        ingredientes: muzzarella,
+        precio: 500
+    },
+    {
+        id: 2,
+        nombre: "pizzaJamonymorron",
+        ingredientes: muzzarella, jamon, morron, 
+        precio: 900
+    },
+    {
+        id: 3,
+        nombre: "pizzaNapolitana",
+        ingredientes: muzarella, tomate, ajo,
+        precio: 700
+    },
+    {
+        id: 4,
+        nombre: "pizzaAnchoas",
+        ingredientes: muzarella, anchoas,
+        precio: 550
+    },
+    {
+        id: 5,
+        nombre: "pizzaJamon",
+        ingredientes: muzarella, jamon,
+        precio: 800
+    },
+    {
+        id: 6,
+        nombre: "pizzaRuculaytomate",
+        ingredientes: muzarella, rucula, tomate,
+        precio: 1000
+    },
+
+]
+
+
+const miObj = {
+    id: 3,
+    nombre: "pizzanapolitana",
+    precio: 700
+}
+console.log(miObj.nombre)
